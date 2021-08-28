61807688f9331c4fa9248e2dd281c0e637153760
KryptOs92 <micheleroma92@gmail.com>
2021-08-29 01:53:34 +0200

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      TEST APP
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.dev.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  entry: {
+    entry: path.join(__dirname, "src", "index.js"),
+  },
+  resolve: {
+    extensions: ['.ts','.tsx', '...']
+},
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg|png|jpg|gif|ico)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs"
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", //3. Inject styles into DOM
+          "css-loader", //2. Turns css into commonjs
+          "sass-loader" //1. Turns sass into css
+        ]
+      },{
+        test: /\.css$/,
+        use: [
+          "style-loader", //3. Inject styles into DOM
+          "css-loader", //2. Turns css into commonjs
+        ]
+      },
+      
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/preset-env","@babel/preset-react","@babel/preset-typescript"],
+          }
+        }
+      }
+
+    ]
+  }
+};
